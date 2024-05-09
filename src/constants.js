a9bd05735b4e86377978fd8f15823b9224038b3c
Abhishek Kumar <sahniak56@gmail.com>
2024-05-10 02:42:31 +0530

    done

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
 // const BASE_URL = "http://localhost:7777";
-const BASE_URL = "https://google-login-deployment-test.onrender.com";
+// const BASE_URL = "https://google-login-deployment-test.onrender.com";
+const BASE_URL = "https://full-stack-google-login-server.vercel.app";
 
 export { BASE_URL };
